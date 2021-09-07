c7efc05ca192ea593be3cbfc48c3ae0bd0f2f51e
purerosefallen <78877@qq.com>
2021-09-07 11:38:04 +0800

    ocgcore outer

---
+++ AI_core_vs2017solution/build/android/jni/Application.mk
@@ -0,0 +1,7 @@
+APP_ABI := armeabi-v7a
+APP_ABI += x86
+APP_PLATFORM := android-16
+APP_ALLOW_MISSING_DEPS=true
+APP_STL := c++_static
+APP_CPPFLAGS := -Wno-error=format-security -std=c++14 -fpermissive
+APP_OPTIM := release
