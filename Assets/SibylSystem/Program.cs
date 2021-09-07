1173bf2e22b64bb4cf9641a448be5073e838a41c
mercury233 <me@mercury233.me>
2021-09-07 19:41:24 +0800

    create replay dir

--- Assets/SibylSystem/Program.cs
+++ Assets/SibylSystem/Program.cs
@@ -310,6 +310,15 @@ public class Program : MonoBehaviour
             {
             }
 
+        if (!Directory.Exists("replay"))
+            try
+            {
+                Directory.CreateDirectory("replay");
+            }
+            catch
+            {
+            }
+
         var fileInfos = new FileInfo[0];
 
         if (Directory.Exists("expansions"))
